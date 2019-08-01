1b0758c0dca6262bd613176afc007a187cb337f8
Daniel Araújo <daniel.araujorn01@gmail.com>
2019-08-01 09:07:03 -0300

    Telefone com máscara não funciona em input numérico

--- src/App/Header/HeaderComponent.jsx
+++ src/App/Header/HeaderComponent.jsx
@@ -57,7 +57,6 @@ export const HeaderComponent = ({
 					<Grid spacing={2} container>
 						<Grid item xs={12} sm={6} md={4} lg={3}>
 							<TextField
-								type="number"
 								autoFocus
 								value={ownersPhone}
 								onChange={({ target: { value: ownersPhone } }) =>
